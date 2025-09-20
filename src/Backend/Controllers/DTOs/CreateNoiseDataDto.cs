64cc4e7145627b362a835c61a4343f83baa308d2
A1ice-Z <alicezheng888@gmail.com>
2025-09-20 16:37:07 +0200

    feat: :sparkles: Create DTOs for noise data

---
+++ src/Backend/Controllers/DTOs/CreateNoiseDataDto.cs
@@ -0,0 +1,7 @@
+using System.ComponentModel.DataAnnotations;
+using System.Xml;
+
+namespace backend.DTOs;
+public record CreateNoiseDataDto(
+    [Required] double LavgQ3
+);
---
+++ src/Backend/Controllers/DTOs/NoiseDataResponseDto.cs
@@ -0,0 +1,7 @@
+namespace backend.DTOs;
+
+public record NoiseDataResponseDto(
+    Guid Id,
+    double LavgQ3,
+    TimeOnly Time
+);
