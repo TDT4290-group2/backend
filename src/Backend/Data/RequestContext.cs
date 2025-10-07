010d36a510435cbeb500a3751cd2c15b18b63810
A1ice-Z <alicezheng888@gmail.com>
2025-10-07 15:23:54 +0200

    refactor: :sparkles: update requestcontext

--- src/Backend/Data/RequestContext.cs
+++ src/Backend/Data/RequestContext.cs
@@ -2,16 +2,12 @@ using Backend.DTOs;
 
 public class RequestContext
 {
-    public SensorDataRequestDto Request { get; set; }
-    public Guid UserId { get; set; }
-    public string DataType { get; set; }
+    public SensorDataRequestDto? Request { get; set; }
+    public Guid? UserId { get; set; }
+    public string? DataType { get; set; }
 
     public void Initialize(SensorDataRequestDto request, Guid userId, string dataType)
     {
-        ArgumentNullException.ThrowIfNull(request);
-        ArgumentNullException.ThrowIfNull(userId);
-        ArgumentNullException.ThrowIfNull(dataType);
-
         Request = request;
         UserId = userId;
         DataType = dataType;
