51248bb797ff2e7fdaefd4c94172dde83bffc1c8
Nathaniavm <nathaniavm@outlook.com>
2025-10-08 13:57:44 +0200

    feat: Connect the backend user logic to database

--- src/Backend/Data/AppDbContext.cs
+++ src/Backend/Data/AppDbContext.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Backend.Records;
+using Backend.Models;
 
 public class AppDbContext : DbContext
 {
@@ -8,4 +9,11 @@ public class AppDbContext : DbContext
     public DbSet<NoiseData> NoiseData { get; set; }
     public DbSet<DustData> DustData { get; set; }
     public DbSet<VibrationData> VibrationData { get; set; }
+    public DbSet<User> User { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfiguration(new Backend.Data.Configuration.UserConfiguration());
+    }
 }
